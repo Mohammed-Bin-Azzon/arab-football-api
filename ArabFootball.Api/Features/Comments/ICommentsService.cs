e378fcd0b4705daaa0db9bc4d1619d6fc8f9027a
Abdualrhman Ghazi <abdualrhman.ghazi@gmail.com>
2026-04-02 11:14:55 +0400

    Add feature Comment in Posts

---
+++ ArabFootball.Api/Features/Comments/ICommentsService.cs
@@ -0,0 +1,10 @@
+﻿using ArabFootball.Api.Features.Comments.CommentsDto;
+
+namespace ArabFootball.Api.Features.Comments
+{
+    public interface ICommentsService
+    {
+        Task<CommentDto?> AddCommentAsync(CreateCommentDto dto);
+        Task<List<CommentDto>> GetPostCommentsAsync(int postId);
+    }
+}
